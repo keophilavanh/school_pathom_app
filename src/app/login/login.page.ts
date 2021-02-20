9a436fbece3bb524fd2a46e22de3070154f15b2d
Thilad <skilled.700@gmail.com>
2021-02-20 17:07:19 +0700

    commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Login } from '../app.model/auth.model';
+import {  Authentication} from "../app.services/auth.services";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth:Authentication,
+    public route:Router,
+    public toastController: ToastController
+    ) { }
 
   ngOnInit() {
   }
 
+
+  login(){
+    const logindata= new Login;
+    logindata.username='admin'
+    logindata.password='12345'
+    console.log('logdata',logindata);
+    this.auth.get_login(logindata).subscribe(
+      (res)=>{
+        console.log(res);
+        if(res.status==1){
+          this.route.navigate(['/tabs/tab1']) 
+        this.succeslogin()
+
+        }else{
+        this.falselogin()
+
+        }
+      },(error)=>{
+        console.log(error);
+      })
+  }
+  
+  async succeslogin() {
+    const toast = await this.toastController.create({
+      message: 'ເຂົ້າສູ່ລະບົບສຳເລັດ',
+      duration: 2000
+    });
+        
+        toast.present();
+
+  }
+  async falselogin() {
+    const toast = await this.toastController.create({
+     message: 'ເຂົ້າສູ່ລະບົບບໍ່ສຳເລັດ ລະຫັກຜ່ານຫຼືຄິວອາໝົກອາຍຸ',
+      duration: 5000
+    });
+    toast.present();
+  }
 }
